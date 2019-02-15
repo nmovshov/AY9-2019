e90ce8cbaca9811fdec874129353e36eca464b6e
Naor Movshovitz <nmovshov@gmail.com>
2019-02-15 11:49:31 -0800

    generators module

--- src/planet_generators.py
+++ src/planet_generators.py
@@ -1,239 +1,51 @@
+"""A small collection of density profile generators with consistent syntax."""
+
 from __future__ import division
 import sys
 import numpy as np
 
-def tripquad_reftop(N, x, ref):
-    """Return piecewise-quadratic density profile stitched to reference density.
+def piecewise_quadratic_planet(N, x):
+    """Piecewise quadratic parameterization of planet's density profile.
 
-    tripquad_reftop(N, x, ref) returns an (N+n)-point density profile, i.e. a
+    piecewise_quadratic_planet(N, x) returns an N-point density profile, i.e. a
     (zvec,dvec) tuple, where the density D is approximated by a
-    piecewise-quadratic function in the normalized radius Z except for the
-    uppermost n levels which instead follow the reference density structure given
-    by ref. The n-by-2 array ref defines the density, ref[:,1], on the level radii
-    ref[:,0]. For the rest of the planet, each quadratic segment is defined by its
-    end points and a curvature coefficient. The segment breakpoints are at z1
-    (upper) and z2 (lower). The density at z1 is d11 (the right-limit) and d21
-    the left-limit). The density at z2 is d22 (the right-limit) and d32 (the
-    left-limit). The density at z=0 is d33. The uppermost segment passes through
-    the point (z,d)=ref[-1,:]. This leaves 10 free parameters. They are ordered
-    as follows:
+    piecewise-quadratic function in the normalized mean radius Z=r/R_m. Each
+    quadratic segment is defined by its end points and a curvature coefficient.
+    The segment breakpoints are at z1 (upper) and z2 (lower). The density at z=1
+    is d10 (and is usually zero). The density at z1 is d11 (the right-limit) and
+    d21 the left-limit). The density at z2 is d22 (the right-limit) and d32 (the
+    left-limit). The density at z=0 is d33. The curvatures of the segments are a1,
+    a2, and a3. The input parameters are ordered as follows:
 
-        x = [a1, y11, a2, y21, y22, a3, y32, y33, lz1, lz2]
+        x = [a1, y10, y11, a2, y21, y22, a3, y32, y33, z1, z2]
     where
-        a1: curvature of first segment (a1*x^2 + b1*x + c1)
-        y11: d11
-        a2: curvature of second segment (a2*x^2 + b2*x + c2)
-        y21: log(d21 - d11)
-        y22: log(d22 - d21)
-        a3: curvature of third segment (a3*x^2 + b3*x + c3)
-        y32: log(d32 - d22)
-        y33: log(d33 - d32)
-        lz1: log(z1) - log(1 - z1)
-        lz2: log(z2) - log(z1 - z2)
+        a1: curvature of first segment (upper envelope)
+        y10: d10 - 0.0 (usually zero, or the 1-bar density)
+        y11: d11 - d10
+        a2: curvature of second segment (lower envelope)
+        y21: d21 - d11
+        y22: d22 - d21
+        a3: curvature of third segment (core)
+        y32: d32 - d22
+        y33: d33 - d32
+        z1: normalized mean radius of first (upper) break point
+        z2: normalized mean radius of second (lower) break point
 
     NOTE: The densities must be specified in real units and the resulting model
-    should have approximately the correct mass without need for renormalization.
-    This is because the reference part of the planet (above Z0) cannot change.
-"""
+    should have approximately the correct mass when the normalized radii are
+    multiplied by planet's outer mean radius.
+    """
 
-    # Minimal input control (some day)
-    assert np.isscalar(N)
+    # Some minimal input control
+    assert np.isscalar(N) and N > 0, "Input 1 should be positive scalar (N)"
     x = np.array(x)
-    assert len(x.shape) == 1
-    assert ref.shape[1] == 2
-
-    # Local variables
-    s0 = ref[0,0]
-    zref = ref[:,0]/s0
-    zvec = np.linspace(zref[-1] - 1/N, 1/N, N)
-    svec = zvec*s0
-
-    # Interpreting the parameters
-    a1 = x[0]; y11 = x[1]
-    a2 = x[2]; y21 = x[3]; y22 = x[4]
-    a3 = x[5]; y32 = x[6]; y33 = x[7]
-    lz1 = x[8]; lz2 = x[9]
-
-    bexp = lambda x: min(np.exp(x), 1e307) # bounded exp to avoid inf/inf=nan
-    d10 = ref[-1,1]
-    d11 = y11
-    d21 = d11 + bexp(y21)
-    d22 = d21 + bexp(y22)
-    d32 = d22 + bexp(y32)
-    d33 = d32 + bexp(y33)
-    r0 = zref[-1]
-    rt = bexp(lz1)/(1 + bexp(lz1))
-    rc = rt*bexp(lz2)/(1 + bexp(lz2))
-
-    # Upper envelope region
-    b1 = (d11 - d10)/(rt - r0) - a1*(rt + r0)
-    c1 = d10 - a1*r0**2 - b1*r0
-
-    # Lower envelope region
-    b2 = (d22 - d21)/(rc - rt) - a2*(rc + rt)
-    c2 = d21 - a2*rt**2 - b2*rt
-
-    # Core region
-    b3 = (d32 - d33)/rc - a3*rc
-    c3 = d33
-
-    # Write the profile
-    dvec = np.zeros(N)
-    for k in range(N):
-        if zvec[k] > rt:
-            dvec[k] = a1*zvec[k]**2 + b1*zvec[k] + c1
-        elif zvec[k] > rc:
-            dvec[k] = a2*zvec[k]**2 + b2*zvec[k] + c2
-        else:
-            dvec[k] = a3*zvec[k]**2 + b3*zvec[k] + c3
-
-    # Return
-    svec = np.hstack((ref[:,0], svec))
-    dvec = np.hstack((ref[:,1], dvec))
-    return (svec, dvec)
-
-def tripquad_quartictop(N, x, z0, R_m, quart):
-    """Return piecewise-quadratic profile stitched to a quartic atmosphere.
-
-    tripquad_quartictop(N, x, z0, R_m, quart) returns an N-point density profile,
-    i.e. a (zvec,dvec) tuple, where the density D is approximated by a
-    piecewise-quadratic function in the normalized radius Z=r/R_m up to Z=z0
-    and follows a quartic polynomial above z0. The array quart holds quartic
-    coefficients in descending powers. Inside of z0, each quadratic segment is
-    defined by its end points and a curvature coefficient. The segment breakpoints
-    are at z1 (upper) and z2 (lower). The density at z0 is d10. The density at z1
-    is d11 (the right-limit) and d21 the left-limit). The density at z2 is d22
-    (the right-limit) and d32 (the left-limit). The density at z=0 is d33. The 11
-    free parameters are ordered as follows:
-
-        x = [a1, y10, y11, a2, y21, y22, a3, y32, y33, lz1, lz2]
-    where
-        a1: curvature of first segment (a1*x^2 + b1*x + c1)
-        y10: d10
-        y11: log(d11 - d10)
-        a2: curvature of second segment (a2*x^2 + b2*x + c2)
-        y21: log(d21 - d11)
-        y22: log(d22 - d21)
-        a3: curvature of third segment (a3*x^2 + b3*x + c3)
-        y32: log(d32 - d22)
-        y33: log(d33 - d32)
-        lz1: log(z1) - log(1 - z1)
-        lz2: log(z2) - log(1 - z2)
-
-    NOTE: The densities must be specified in real units and the resulting model
-    should have approximately the correct mass without need for renormalization.
-    This is because the reference part of the planet (above Z0) cannot change.
-"""
-
-    # Minimal input control (some day)
-    assert np.isscalar(N)
-    assert np.isscalar(z0)
-    assert np.isscalar(R_m)
-    x = np.array(x)
-    quart = np.array(quart)
-    assert len(x.shape) == 1
-    assert len(quart.shape) == 1
-
-    # Local variables
-    zvec = np.linspace(1, 1/N, N)
-    svec = zvec*R_m
+    assert x.ndim == 1 and len(x) == 11, "Input 2 should be 11-vector (x)"
 
     # Interpreting the parameters
     a1 = x[0]; y10 = x[1]; y11 = x[2]
     a2 = x[3]; y21 = x[4]; y22 = x[5]
     a3 = x[6]; y32 = x[7]; y33 = x[8]
-    lz1 = x[9]; lz2 = x[10]
-
-    bexp = lambda x: min(np.exp(x), 1e307) # bounded exp to avoid inf/inf=nan
-    d10 = y10
-    d11 = d10 + bexp(y11)
-    d21 = d11 + bexp(y21)
-    d22 = d21 + bexp(y22)
-    d32 = d22 + bexp(y32)
-    d33 = d32 + bexp(y33)
-    r0 = z0
-    rt = bexp(lz1)/(1 + bexp(lz1))
-    rc = bexp(lz2)/(1 + bexp(lz2))
-
-    # Upper envelope region
-    b1 = (d11 - d10)/(rt - r0) - a1*(rt + r0)
-    c1 = d10 - a1*r0**2 - b1*r0
-
-    # Lower envelope region
-    b2 = (d22 - d21)/(rc - rt) - a2*(rc + rt)
-    c2 = d21 - a2*rt**2 - b2*rt
-
-    # Core region
-    b3 = (d32 - d33)/rc - a3*rc
-    c3 = d33
-
-    # Write the profile
-    dvec = np.zeros(N)
-    for k in range(N):
-        if zvec[k] > r0:
-            dvec[k] = d10*np.polyval(quart, zvec[k])
-        elif zvec[k] > rt:
-            dvec[k] = a1*zvec[k]**2 + b1*zvec[k] + c1
-        elif zvec[k] > rc:
-            dvec[k] = a2*zvec[k]**2 + b2*zvec[k] + c2
-        else:
-            dvec[k] = a3*zvec[k]**2 + b3*zvec[k] + c3
-
-    # Return
-    return (svec, dvec)
-
-def pppp_quartop(N, x, z0, R_m, quart):
-    """Preferred Piecewise Polynomial Parameterization, with a quartic top.
-
-    pppp_quartop(N, x, z0, R_m, quart) returns an N-point density profile,
-    i.e. a (zvec,dvec) tuple, where the density D is approximated by a
-    piecewise-quadratic function in the normalized radius Z=r/R_m up to Z=z0
-    and follows a quartic polynomial above z0. The array quart holds quartic
-    coefficients in descending powers. Inside of z0, each quadratic segment is
-    defined by its end points and a curvature coefficient. The segment breakpoints
-    are at z1 (upper) and z2 (lower). The density at z0 is d10. The density at z1
-    is d11 (the right-limit) and d21 the left-limit). The density at z2 is d22
-    (the right-limit) and d32 (the left-limit). The density at z=0 is d33. The
-    curvature of the innermost segment is assumed to be zero (i.e. a linear core).
-    The 10 remaining free parameters are ordered as follows:
-
-        x = [a1, y10, y11, a2, y21, y22, <a3=0>, y32, y33, lz1, lz2]
-    where
-        a1: curvature of first segment (a1*x^2 + b1*x + c1)
-        y10: d10
-        y11: d11 - d10
-        a2: curvature of second segment (a2*x^2 + b2*x + c2)
-        y21: d21 - d11
-        y22: d22 - d21
-        <a3 would be here but it's always zero>
-        y32: d32 - d22
-        y33: d33 - d32
-        lz1: z1
-        lz2: z2
-
-    NOTE: The densities must be specified in real units and the resulting model
-    should have approximately the correct mass without need for renormalization.
-    This is because the reference part of the planet (above Z0) cannot change.
-"""
-
-    # Minimal input control (some day)
-    assert np.isscalar(N)
-    assert np.isscalar(z0)
-    assert np.isscalar(R_m)
-    x = np.array(x)
-    quart = np.array(quart)
-    assert len(x.shape) == 1
-    assert len(quart.shape) == 1
-
-    # Local variables
-    zvec = np.linspace(1, 1/N, N)
-    svec = zvec*R_m
-
-    # Interpreting the parameters
-    a1 = x[0]; y10 = x[1]; y11 = x[2]
-    a2 = x[3]; y21 = x[4]; y22 = x[5]
-    a3 = 0.00; y32 = x[6]; y33 = x[7]
-    lz1 = x[8]; lz2 = x[9]
+    z1 = x[9]; z2 = x[10]
 
     d10 = y10
     d11 = d10 + y11
@@ -241,44 +53,37 @@ def pppp_quartop(N, x, z0, R_m, quart):
     d22 = d21 + y22
     d32 = d22 + y32
     d33 = d32 + y33
-    r0 = z0
-    rt = lz1
-    rc = lz2
 
     # Upper envelope region
-    b1 = (d11 - d10)/(rt - r0) - a1*(rt + r0)
-    c1 = d10 - a1*r0**2 - b1*r0
+    b1 = (d11 - d10)/(z1 - 1.0) - a1*(z1 + 1.0)
+    c1 = d10 - a1 - b1
 
     # Lower envelope region
-    b2 = (d22 - d21)/(rc - rt) - a2*(rc + rt)
-    c2 = d21 - a2*rt**2 - b2*rt
+    b2 = (d22 - d21)/(z2 - z1) - a2*(z2 + z1)
+    c2 = d21 - a2*z1**2 - b2*z1
 
     # Core region
-    b3 = (d32 - d33)/rc - a3*rc
+    b3 = (d32 - d33)/z2 - a3*z2
     c3 = d33
 
     # Write the profile
+    zvec = np.linspace(1, 1/N, N)
     dvec = np.zeros(N)
     for k in range(N):
-        if zvec[k] > r0:
-            dvec[k] = d10*np.polyval(quart, zvec[k])
-        elif zvec[k] > rt:
+        if zvec[k] > z1:
             dvec[k] = a1*zvec[k]**2 + b1*zvec[k] + c1
-        elif zvec[k] > rc:
+        elif zvec[k] > z2:
             dvec[k] = a2*zvec[k]**2 + b2*zvec[k] + c2
         else:
             dvec[k] = a3*zvec[k]**2 + b3*zvec[k] + c3
 
     # Return
-    return (svec, dvec)
+    return (zvec, dvec)
 
 def mass(svec, dvec):
     dro = np.hstack((dvec[0], np.diff((dvec))))
     m = sum(dro*(svec)**3)
     return 4*np.pi/3*m
 
-def _test():
-    print("alo world")
-
 if __name__ == '__main__':
-    _test()
+    print("alo world")
